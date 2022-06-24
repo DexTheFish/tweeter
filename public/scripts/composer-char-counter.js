9757a21175fc9c24c68ad940ff560fee8b3b29e4
Declan Davis <dexthefish@gmail.com>
2022-06-23 23:54:28 -0400

    linting and removing unnecessary comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,7 @@
 $(document).ready(function() { //triggers callback function when DOM has finished loading
   $("textarea").on('input', function() { //look for input to a DOM node with selector "textarea"
     const tweetElement = this; // this refers to an html element (<textarea>..</textarea>)
-    const tweetObject  = $(this); //$(this) refers to its corresponding jQuery object
+    const tweetObject  = $(tweetElement); //$(this) refers to its corresponding jQuery object
     const tweetLength  = tweetObject.val().length;
     let remainingChars = 140 - tweetLength;
     // access the counter by tree traversal:
@@ -19,10 +19,4 @@ $(document).ready(function() { //triggers callback function when DOM has finishe
       counterObject.removeClass("long");
     }
   });
-  //console.log('test2', $("textarea"));
-
-
-
-
-
 });
